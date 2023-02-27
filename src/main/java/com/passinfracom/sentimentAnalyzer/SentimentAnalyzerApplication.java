bf617df62a39dfa21955a86702788e6b4886cca3
Shamik <mitrashamik@gmail.com>
2023-02-27 15:52:00 +0530

    Update OpenApi desc

--- src/main/java/com/passinfracom/sentimentAnalyzer/SentimentAnalyzerApplication.java
+++ src/main/java/com/passinfracom/sentimentAnalyzer/SentimentAnalyzerApplication.java
@@ -9,7 +9,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Sentiment Analysis API", version = "1.0", description = "Sentiment Analyzer"))
+@OpenAPIDefinition(info = @Info(title = "Sentiment Analysis API with chatGPT", version = "1.0", description = "Sentiment Analyzer"))
 public class SentimentAnalyzerApplication {
 
 	public static void main(String[] args) {
