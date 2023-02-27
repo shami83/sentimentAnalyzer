ff3bf0e46b264694fff0b0455a4c48e15b718999
Swastika Basu <swastika.basu@ibm.com>
2023-02-27 10:50:27 +0530

    enabling swagger

--- src/main/java/com/passinfracom/sentimentAnalyzer/SentimentAnalyzerApplication.java
+++ src/main/java/com/passinfracom/sentimentAnalyzer/SentimentAnalyzerApplication.java
@@ -5,13 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Sentiment Analysis API", version = "1.0", description = "Sentiment Analyzer"))
 public class SentimentAnalyzerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SentimentAnalyzerApplication.class, args);
 	}
-	
+
 	@Bean
 	RestTemplate creteRestTemplet() {
 		return new RestTemplate();
